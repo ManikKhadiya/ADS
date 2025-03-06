879f7e1c146691bf6531649c60c12ce118576876
Manik Khadiya <manikkhadiya@gmail.com>
2025-03-06 17:44:13 +0000

    lab work

---
+++ src/primitiveSize.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main () {
+    int integerVariable;
+    size_t size;
+    size = sizeof(integerVariable);
+    printf ("Size of Integer Variable is % zu bytes \n", size);
+    return 0;
+}
---
+++ src/primitiveSizeCompact.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+int main (){
+    int integerVariable;
+    printf("Size of Integer Variable is % zu bytes \n", sizeof(integerVariable));
+    return 0;
+}
